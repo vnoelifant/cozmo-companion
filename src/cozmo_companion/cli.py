e49a16534b39857a41ca5c3cf69f078d73a302f6
vnoelifant <veronicanoel12490@gmail.com>
2023-05-29 14:30:58 -0500

    refactor

--- src/cozmo_companion/cli.py
+++ src/cozmo_companion/cli.py
@@ -3,62 +3,33 @@ import os
 import sys
 import traceback
 
-import openai
 import typer
 
 from decouple import config
 
-from .dialogue_cozmo import Dialogue
-from .recorder_cozmo import Recorder
+from .dialogue_cozmo import VoiceAssistant
 
 app = typer.Typer()
-
-openai.api_key = config("OPENAI_API_KEY")
-
-EXIT_CONDITION: str = "exit"
-GPT_MODEL: str = "gpt-3.5-turbo"
-max_tokens: int = 1000
-temperature: int = 1.2
-
-
-def get_completion(prompt, model=GPT_MODEL):
-    messages = [{"role": "user", "content": prompt}]
-
-    gpt_response = openai.ChatCompletion.create(
-        model=GPT_MODEL,
-        max_tokens=max_tokens,
-        temperature=temperature,
-        messages=messages,
-    )
-
-    gpt_response_msg = gpt_response["choices"][0]["message"]["content"]
-
-    return gpt_response_msg
 
 
 @app.command()
 def converse(filename: str):
-    curr_dir = os.getcwd()
-    speech_out = os.path.join(curr_dir, "wav_output", filename + ".wav")
 
-    my_convo = Dialogue(speech_out)
-    my_convo.get_cozmo_response(
+    EXIT_CONDITION: str = "exit"
+
+    assistant = VoiceAssistant()
+    assistant.speak(
         "Hello! Ask GPT anything and I will echo back what it says in response!"
     )
 
     while True:
         try:
-            print("starting recording process")
-            recorder = Recorder(speech_out)
-            print("Please say something nice into the microphone\n")
-            recorder.save_to_file()
-            print("Transcribing audio....\n")
 
-            speech_text = my_convo.transcribe_audio()
+            speech_text = assistant.listen(filename)
 
-            gpt_response_msg = get_completion(speech_text)
+            gpt_response_msg = assistant.get_gpt_completion(speech_text)
 
-            my_convo.get_cozmo_response(gpt_response_msg)
+            assistant.speak(gpt_response_msg)
 
             if speech_text in EXIT_CONDITION:
                 print("Exiting program...")
--- src/cozmo_companion/dialogue_cozmo.py
+++ src/cozmo_companion/dialogue_cozmo.py
@@ -3,38 +3,68 @@ import json
 import sys
 import time
 import traceback
+import os
 
 import cozmo
 import anki_vector
+import openai
 from decouple import config
 from ibm_cloud_sdk_core.authenticators import IAMAuthenticator
 from ibm_watson import SpeechToTextV1
 from PIL import Image
+from .recorder_cozmo import Recorder
+
+# Initialize speech to text service. Source: https://cloud.ibm.com/apidocs/speech-to-text
+authenticator = IAMAuthenticator(config("IAM_APIKEY_STT"))
+
+speech_to_text = SpeechToTextV1(authenticator=authenticator)
+
+speech_to_text.set_service_url(config("URL_STT"))
+
+# Configure Open AI API KEY
+openai.api_key = config("OPENAI_API_KEY")
 
 
-class Dialogue:
-    def __init__(self, path_to_audio_file):
-        self.path_to_audio_file = path_to_audio_file
+class VoiceAssistant:
 
-    def transcribe_audio(self):
+    """
+    A class to respresent a voice assistant who can listen to voice input, provide a gpt response,
+    and speak the gpt response
+    """
+
+    def __init__(self):
+        self.content_type = "audio/wav"
+        self.word_alternatives_threshold = 0.9
+        self.keywords = ["hey", "hi", "watson", "friend", "meet"]
+        self.keywords_threshold = 0.5
+        self.gpt_model = "gpt-3.5-turbo"
+        self.max_tokens = 1000
+        self.temperature = 1.2
+        self.conversation_history = [
+            {"role": "system", "content": "You are a helpful, friendly assistant"}
+        ]
+
+    def listen(self, filename):
         """
-        Function to transcribe recorded speech
+        Function to record audio and transcribe recorded speech
         """
+        curr_dir = os.getcwd()
+        speech_out = os.path.join(curr_dir, "wav_output", filename + ".wav")
 
-        # initialize speech to text service. Source: https://cloud.ibm.com/apidocs/speech-to-text
-        authenticator = IAMAuthenticator(config("IAM_APIKEY_STT"))
+        print("starting recording process")
+        recorder = Recorder(speech_out)
 
-        speech_to_text = SpeechToTextV1(authenticator=authenticator)
+        print("Please say something nice into the microphone\n")
+        recorder.save_to_file()
+        print("Transcribing audio....\n")
 
-        speech_to_text.set_service_url(config("URL_STT"))
-
-        with open((self.path_to_audio_file), "rb") as audio_file:
+        with open((speech_out), "rb") as audio_file:
             speech_result = speech_to_text.recognize(
                 audio=audio_file,
-                content_type="audio/wav",
-                word_alternatives_threshold=0.9,
-                keywords=["hey", "hi", "watson", "friend", "meet"],
-                keywords_threshold=0.5,
+                content_type=self.content_type,
+                word_alternatives_threshold=self.word_alternatives_threshold,
+                keywords=self.keywords,
+                keywords_threshold=self.keywords_threshold,
             ).get_result()
 
             speech_text = speech_result["results"][0]["alternatives"][0]["transcript"]
@@ -42,12 +72,38 @@ class Dialogue:
 
         return speech_text
 
-    def get_cozmo_response(self, response):
+    def get_gpt_completion(self, text):
         """
-        Function to get Cozmo's response
-        top_tone parameter was used for integration of Watson Tone Analyzer, which is depracated
+        Function to generate a GPT response to the user's text input
+        """
+
+        # Add the user's input to the Chat GPT history log
+        self.conversation_history.append({"role": "user", "content": text})
+
+        gpt_response = openai.ChatCompletion.create(
+            model=self.gpt_model,
+            max_tokens=self.max_tokens,
+            temperature=self.temperature,
+            messages=self.conversation_history,
+        )
+
+        gpt_response_msg = gpt_response.choices[0].message.content
+
+        self.conversation_history.append(
+            {
+                "role": gpt_response_msg.choices[0].message.role,
+                "content": gpt_response_msg,
+            }
+        )
+        print("GPT RESPONSE MESSAGE: ", gpt_response_msg)
+
+        return gpt_response_msg
+
+    def speak(self, gpt_response):
+        """
+        Function to get Cozmo to convert GPT text response to speech
         """
 
         # Cozmo speaks the text response from Watson
         with anki_vector.Robot() as robot:
-            robot.behavior.say_text(response)
+            robot.behavior.say_text(gpt_response)
