7ac7a46f040a99c5d909d22d6febcd43495c3b98
vnoelifant <veronicanoel12490@gmail.com>
2023-10-26 21:34:10 -0500

    adding Marvin classifier and tools

--- src/cozmo_companion/assistant.py
+++ src/cozmo_companion/assistant.py
@@ -6,9 +6,11 @@ from datetime import datetime
 from decouple import config
 from ibm_cloud_sdk_core.authenticators import IAMAuthenticator
 from ibm_watson import SpeechToTextV1, TextToSpeechV1, ApiException
-from marvin import AIApplication
+from marvin import AIApplication, ai_classifier
+from marvin.tools import tool
 from pydub import AudioSegment
 from pydub.playback import play
+from enum import Enum
 
 from .constants import (
     AUDIO_FORMAT,
@@ -19,6 +21,11 @@ from .constants import (
     WORD_ALTERNATIVE_THRESHOLDS,
 )
 from .recorder import Recorder
+
+@ai_classifier
+class Sentiment(Enum):
+    POSITIVE = "POSITIVE"
+    NEGATIVE = "NEGATIVE"
 
 
 class VoiceAssistant:
@@ -32,10 +39,11 @@ class VoiceAssistant:
         """Initialize the VoiceAssistant and its services."""
         self._configure_services()
         self.chatbot = AIApplication(description=("A friendly, supportive chatbot."
-                                                  "If it detects negative sentimentm it provides an"
-                                                  "empathetic response to the user. Then, it asks the user "
-                                                  "how it can make the user feel better. When the user responds," 
-                                                  "it provides a response based on the user's preference. "))
+                                                  "It always provides an empathetic response when it detects"
+                                                  "negative sentiment. For every response to the user, it"
+                                                  "asks the user if its response helped."
+                                                 ),
+                                                 tools=[self.get_feedback_inquiry])
 
         self.conversation_history = []
 
@@ -82,6 +90,7 @@ class VoiceAssistant:
         speech_file = os.path.join(os.getcwd(), "wav_output", filename)
 
         return speech_file
+    
 
     def _listen(self):
         """Record audio and transcribe the recorded speech."""
@@ -118,27 +127,39 @@ class VoiceAssistant:
                 "Method failed with status code " + str(ex.code) + ": " + ex.message
             )
 
+    @tool
+    def get_feedback_inquiry(self, user_text):
+        feedback_phrases = ["joke", "motivational quote"]
+        for phrase in feedback_phrases:
+            if phrase in user_text.lower():
+                return " Did my response help?"
+        return None
+    
+    def construct_gpt_prompt(self, text):
+        """Construct the GPT-3 prompt based on the user's sentiment."""
+        detected_sentiment = Sentiment(text)
+        if detected_sentiment == Sentiment.NEGATIVE:
+            return text + " Requesting an empathetic response."
+        return text
+
     def _generate_response(self, text):
         """Generate a GPT response to the user's text input."""
-
         try:
-            # Add the user's input to the Chat GPT history log
             self.conversation_history.append({"role": "user", "content": text})
 
-            gpt_response = self.chatbot(text)
+            gpt_prompt = self.construct_gpt_prompt(text)
+            gpt_response_content = self.chatbot(gpt_prompt).content
 
-            gpt_response_msg = gpt_response.content
+            # Check for negative sentiment before appending feedback
+            if "Requesting an empathetic response." in gpt_prompt:
+                feedback_inquiry = self.get_feedback_inquiry(text)
+                if feedback_inquiry:
+                    gpt_response_content += feedback_inquiry
 
-            self.conversation_history.append(
-                {
-                    "role": "gpt",
-                    "content": gpt_response_msg,
-                }
-            )
-
+            self.conversation_history.append({"role": "gpt", "content": gpt_response_content})
             print("Conversation Log: ", self.conversation_history)
 
-            return gpt_response_msg
+            return gpt_response_content
         except Exception as e:
             logging.error(f"Error getting GPT completion: {e}", exc_info=True)
             return "I'm sorry, I couldn't process that."
--- src/cozmo_companion/constants.py
+++ src/cozmo_companion/constants.py
@@ -2,7 +2,6 @@ CONTENT_TYPE = "audio/wav"
 WORD_ALTERNATIVE_THRESHOLDS = 0.9
 KEYWORDS = ["hey", "hi", "watson", "friend", "meet"]
 KEYWORDS_THRESHOLD = 0.5
-GPT_MODEL = "gpt-4"
 MAX_TOKENS = 1000
 TEMPERATURE = 1.2
 AUDIO_FORMAT = "audio/wav"
