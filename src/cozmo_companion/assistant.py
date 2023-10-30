76102b37ebe1a68bf382aaf78220e97a69b1de83
vnoelifant <veronicanoel12490@gmail.com>
2023-10-30 00:27:28 -0500

    comments added

--- src/cozmo_companion/assistant.py
+++ src/cozmo_companion/assistant.py
@@ -24,6 +24,7 @@ from .constants import (
 from .recorder import Recorder
 
 
+# Enum class for sentiment analysis
 @ai_classifier
 class Sentiment(Enum):
     POSITIVE = "POSITIVE"
@@ -40,6 +41,7 @@ class VoiceAssistant:
     def __init__(self):
         """Initialize the VoiceAssistant and its services."""
         self._configure_services()
+        # Setting up the chatbot with description and tools
         self.chatbot = AIApplication(
             description=(
                 "A friendly, supportive chatbot."
@@ -49,17 +51,19 @@ class VoiceAssistant:
             ),
             tools=[self.get_feedback_inquiry],
         )
-
+        # Initializing conversation history to store user and bot interactions
         self.conversation_history = []
 
     def _configure_services(self):
         """Configure and initialize external services (IBM, Marvin, etc.)."""
+        # Initialize IBM services for speech-to-text and text-to-speech
         self.SPEECH_TO_TEXT = self._initialize_ibm_service(
             config("IAM_APIKEY_STT"), config("URL_STT")
         )
         self.TEXT_TO_SPEECH = self._initialize_ibm_service(
             config("IAM_APIKEY_TTS"), config("URL_TTS")
         )
+        # Setting up Marvin settings
         marvin.settings.openai.api_key = config("OPENAI_API_KEY")
         marvin.settings.llm_model = config("MARVIN_LLM_MODEL")
 
@@ -70,8 +74,10 @@ class VoiceAssistant:
         :param url: The URL for the service.
         :return: Initialized IBM service.
         """
+        # Creating an IAM authenticator using the provided API key
         authenticator = IAMAuthenticator(api_key)
         service = None
+        # Determine the type of service based on the URL and initialize it
         if "speech-to-text" in url:
             service = SpeechToTextV1(authenticator=authenticator)
         elif "text-to-speech" in url:
@@ -86,6 +92,7 @@ class VoiceAssistant:
     @staticmethod
     def _create_wav_file(prefix=""):
         """Create a WAV file in the designated directory based on timestamp."""
+        # Check if the directory exists, if not create it
         if not os.path.exists("wav_output"):
             os.makedirs("wav_output")
 
@@ -98,19 +105,19 @@ class VoiceAssistant:
 
     def _listen(self):
         """Record audio and transcribe the recorded speech."""
-
+        # Create a WAV file to store the user's speech
         user_speech_file = VoiceAssistant._create_wav_file(prefix="user")
 
         print("Starting recording process")
-
+        # Initialize the recorder
         recorder = Recorder(user_speech_file)
 
         print("Please say something to the microphone\n")
-
+        # Start recording
         recorder.record()
 
         print("Transcribing audio....\n")
-
+        # Transcribe the recorded audio using IBM's Speech-to-Text service
         try:
             with open((user_speech_file), "rb") as audio:
                 speech_result = self.SPEECH_TO_TEXT.recognize(
@@ -120,12 +127,13 @@ class VoiceAssistant:
                     keywords=KEYWORDS,
                     keywords_threshold=KEYWORDS_THRESHOLD,
                 ).get_result()
-
+                # Extract the transcribed text from the result
                 user_speech_text = speech_result["results"][0]["alternatives"][0][
                     "transcript"
                 ]
 
             return user_speech_text
+        # Handle exceptions from the IBM service
         except ApiException as ex:
             logging.error(
                 "Method failed with status code " + str(ex.code) + ": " + ex.message
@@ -143,15 +151,20 @@ class VoiceAssistant:
         Returns:
             Optional[str]: The feedback inquiry string or None if no match is found.
         """
+        # List of feedback phrases to check against
         feedback_phrases = ["joke", "motivational quote"]
         for phrase in feedback_phrases:
+            # If the user's text contains any of the feedback phrases, return the inquiry string
             if phrase in user_text.lower():
                 return " Did this help put a smile to your face?"
+        # If no match is found, return None
         return None
 
     def construct_gpt_prompt(self, text):
         """Construct the GPT-3 prompt based on the user's sentiment."""
+        # Detect the sentiment of the user's text
         detected_sentiment = Sentiment(text)
+        # If the sentiment is negative, append a request for an empathetic response
         if detected_sentiment == Sentiment.NEGATIVE:
             print("Negative Sentiment Detected...")
             return text + " Requesting an empathetic response."
@@ -160,30 +173,36 @@ class VoiceAssistant:
     def _generate_response(self, text):
         """Generate a GPT response to the user's text input."""
         try:
+            # Construct the GPT prompt based on the input text
             gpt_prompt = self.construct_gpt_prompt(text)
-            
-            self.conversation_history.append({"role": "user", "content": gpt_prompt})
-           
-            feedback_inquiry = self.get_feedback_inquiry(gpt_prompt)
 
-            if feedback_inquiry:
-                self.chatbot(gpt_prompt).content += feedback_inquiry  # Append the feedback inquiry if it exists
-            
-            self.conversation_history.append(
-                {"role": "gpt", "content": self.chatbot(gpt_prompt).content}
-            )
+            # Update the conversation history with the user's input
+            self.conversation_history.append({"role": "user", "content": gpt_prompt})
+
+            # Get the chatbot's response content
+            gpt_response = self.chatbot(gpt_prompt).content
+
+            # If there's a feedback inquiry, append it to the chatbot's response
+            feedback_inquiry = self.get_feedback_inquiry(gpt_prompt)
+            if feedback_inquiry and feedback_inquiry not in gpt_response:
+                gpt_response += feedback_inquiry
+
+            # Update the conversation history with the chatbot's response
+            self.conversation_history.append({"role": "gpt", "content": gpt_response})
+
+            # Print the conversation log (this can be removed if not needed in production)
             print("Conversation Log: ", self.conversation_history)
 
-            return self.chatbot(gpt_prompt).content
+            return gpt_response
         except Exception as e:
             logging.error(f"Error getting GPT completion: {e}", exc_info=True)
             return "I'm sorry, I couldn't process that."
 
     def _speak(self, text):
         """Convert text input to speech."""
-
+        # Create a WAV file to store the bot's speech
         bot_speech_file = VoiceAssistant._create_wav_file(prefix="bot")
-
+        # Convert the text to speech using IBM's Text-to-Speech service
         try:
             with open(bot_speech_file, "wb") as audio_out:
                 audio_out.write(
@@ -195,23 +214,29 @@ class VoiceAssistant:
                     .get_result()
                     .content
                 )
-
+            # Play the generated speech
             bot_speech_response = AudioSegment.from_wav(bot_speech_file)
             play(bot_speech_response)
         except ApiException as ex:
+            # Handle exceptions from the IBM service
             logging.error(
                 "Method failed with status code " + str(ex.code) + ": " + ex.message
             )
 
     def start_session(self):
         """Handle the conversation with the user."""
+        # Start the session by speaking a greeting
         self._speak("Hello! Chat with GPT and I will speak its responses!")
         while True:
+            # Listen to the user's speech and transcribe it
             user_speech_text = self._listen()
             print(f"User Speech Text: {user_speech_text} \n")
+            # Exit the loop if the user says "exit"
             if "exit" in user_speech_text.strip().lower():
                 self._speak("Goodbye!")
                 break
+            # Generate a GPT response for the user's input
             gpt_response_msg = self._generate_response(user_speech_text)
             print(f"GPT Response Message: {gpt_response_msg} \n")
+            # Speak the GPT response
             self._speak(gpt_response_msg)
--- src/cozmo_companion/cli.py
+++ src/cozmo_companion/cli.py
@@ -1,14 +1,26 @@
 import typer
 
+# Importing the VoiceAssistant class from the assistant module
 from .assistant import VoiceAssistant
 
+# Initializing the Typer application for command-line interface
 app = typer.Typer()
 
 
+# Defining a command for the Typer application
 @app.command()
 def converse():
+    """
+    Initialize the voice assistant and start a conversation session.
+    This function creates an instance of the VoiceAssistant and
+    initiates its session. If interrupted with a keyboard command
+    (like Ctrl+C), it provides a graceful exit message.
+    """
+    # Creating an instance of the VoiceAssistant
     assistant = VoiceAssistant()
     try:
+        # Starting the session with the user
         assistant.start_session()
     except KeyboardInterrupt:
+        # Handling keyboard interrupt to gracefully exit the application
         print("\nClosing via keyboard interrupt.")
