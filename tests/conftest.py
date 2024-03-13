ba3a1bec5ed123d9acfa55d96e7bb339d2faed8a
vnoelifant <veronicanoel12490@gmail.com>
2024-03-12 22:58:19 -0500

    added tests and marvin 2.1.6 update

---
+++ tests/conftest.py
@@ -0,0 +1,19 @@
+"""
+import pytest
+
+@pytest.fixture(autouse=True)
+def mock_speech_settings(mocker):
+    mocker.patch('marvin.settings.openai.audio.speech.voice', 'echo')
+
+"""
+
+import pytest
+from decouple import config
+
+
+@pytest.fixture(autouse=True, scope="session")
+def override_voice_variable(monkeypatch):
+    original_voice = config("VOICE")
+    monkeypatch.setenv("VOICE", "echo")  # Set to a valid value for `marvin`
+    yield
+    monkeypatch.setenv("VOICE", original_voice)  # Reset after tests
--- tests/test_assistant.py
+++ tests/test_assistant.py
@@ -1,9 +1,27 @@
 import pytest
+import marvin
 
 from src.cozmo_companion.assistant import (
     is_feedback_inquiry_present,
     get_feedback_inquiry,
+    VoiceAssistant,
 )
+
+
+def test_configure_services(monkeypatch):
+    # Set up the monkeypatch for the environment variables
+    monkeypatch.setenv("MARVIN_OPENAI_API_KEY", "fake_api_key")
+    monkeypatch.setenv("MARVIN_CHAT_COMPLETIONS_MODEL", "fake_model")
+
+    # Create an instance of your class
+    assistant = VoiceAssistant()
+
+    # Invoke the method that configures services
+    assistant._configure_services()
+
+    # Assert that marvin settings were correctly assigned from environment variables
+    assert marvin.settings.openai.api_key.get_secret_value() == "fake_api_key"
+    assert marvin.settings.openai.chat.completions.model == "fake_model"
 
 
 @pytest.mark.parametrize(
@@ -33,7 +51,15 @@ def test_is_feedback_inquiry_present(bot_text, expected):
 @pytest.mark.parametrize(
     "user_request_type, user_sentiment, expected",
     [
-        pytest.param("joke", "positive", "Did that joke make you smile?"),
+        pytest.param(
+            "joke", "positive", "Did that joke make you smile?", id="Positive"
+        ),
+        pytest.param(
+            "joke", "negative", "Did that joke help cheer you up a bit?", id="Negative"
+        ),
+        pytest.param(
+            "joke", "neutral", "What did you think of that joke?", id="Neutral"
+        ),
     ],
 )
 def test_get_feedback_inquiry(user_request_type, user_sentiment, expected):
